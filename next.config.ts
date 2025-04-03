443b06740f53eef0b6144508375364461116b564
Leng Sokchhay <lengsokchhay.168@gmail.com>
2025-04-03 14:56:54 +0700

    Add Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false,
+  output: "standalone",
+  devIndicators: {
+    buildActivity: false,
+  },
 };
 
 export default nextConfig;
-
